e3e88834cfcea61ba1412ce026f28b24f1506d90
Felipe França <felipe.paulino57@gmail.com>
2025-01-26 16:50:11 -0300

    fix: fixes exclusion params for "lock" files

--- push.sh
+++ push.sh
@@ -7,5 +7,5 @@ push() {
   echo -e "Syncing: [$file_name] in local [$target_path] to remote [Projects:$file_name]"
 
   rclone copy "$target_path" "Projects:$file_name" \
-    --exclude "*.swp" --exclude "*~" --exclude "*.tmp" -v || return 1;
+    --exclude "*.swp" --exclude ".~*" --exclude "*.tmp" -v || return 1;
 }
